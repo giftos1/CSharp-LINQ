bd34465bacd9c2c34b6346dc7ab04c22eb29d30b
giftos1 <giftsydney676@gmail.com>
2025-11-29 13:04:57 +1030

    Utilize the methods in the utility class CheckUpperCase

--- LinqIntroApp/Program.cs
+++ LinqIntroApp/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LinqIntroApp;
+
 Console.WriteLine("Hello, World!");
+
+
+var words = new string[]
+{
+    "hello",
+    "WORLD",
+    "Test",
+    "LINQ"
+};
+
+var wordsNoUpper = new string[]
+{
+    "hello",
+    "world",
+    "test",
+    "linq"
+};
+
+//var checkerNoUpper = new CheckUpperCase(wordsNoUpper).IsAnyWordUpperCase();
+
+Console.WriteLine(CheckUpperCase.IsAnyWordUpperCase(words));
+Console.WriteLine(CheckUpperCase.IsAnyWordUpperCaseLinq(words));
+Console.WriteLine(CheckUpperCase.IsAnyWordUpperCase(wordsNoUpper));
+Console.WriteLine(CheckUpperCase.IsAnyWordUpperCaseLinq(wordsNoUpper));
