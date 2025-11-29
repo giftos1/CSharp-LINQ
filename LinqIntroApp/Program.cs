3d837147389ccd0a6564f575f2ccc6c7c9904783
giftos1 <giftsydney676@gmail.com>
2025-11-29 13:25:42 +1030

    Most LINQ methods are extension methods like where

--- LinqIntroApp/Program.cs
+++ LinqIntroApp/Program.cs
@@ -26,3 +26,13 @@ Console.WriteLine(CheckUpperCase.IsAnyWordUpperCase(words));
 Console.WriteLine(CheckUpperCase.IsAnyWordUpperCaseLinq(words));
 Console.WriteLine(CheckUpperCase.IsAnyWordUpperCase(wordsNoUpper));
 Console.WriteLine(CheckUpperCase.IsAnyWordUpperCaseLinq(wordsNoUpper));
+
+
+// LINQ and extension methods
+
+var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+var wordsLongerThan3Letters = words.Where(word => word.Length > 3);
+var oddNumbers = numbers.Where(number => number % 2 == 1);
+
+Console.WriteLine(string.Join(", ", wordsLongerThan3Letters));
+Console.WriteLine(string.Join(", ", oddNumbers));
