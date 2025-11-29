de1ca91b6a2d9d5670e4719a83ccc2b1b9051481
giftos1 <giftsydney676@gmail.com>
2025-11-29 13:02:37 +1030

    Add utility class that uses linq and regular methods

---
+++ LinqIntroApp/CheckUpperCase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqIntroApp
+{
+    public class CheckUpperCase
+    {
+        //private readonly IEnumerable<string> _words;
+
+        //public CheckUpperCase(IEnumerable<string> words)
+        //{
+        //    _words = words;
+        //}
+
+        public static bool IsAnyWordUpperCase(IEnumerable<string> words)
+        {
+            foreach (var word in words)
+            {
+                bool areAllUpperCase = true;
+                foreach (var letter in word)
+                {
+                    if (char.IsLower(letter))
+                    {
+                        areAllUpperCase = false;
+                    }
+                }
+                if (areAllUpperCase)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool IsAnyWordUpperCaseLinq(IEnumerable<string> words)
+        {
+            return words.Any(word => word.All(letter => char.IsUpper(letter)));
+        }
+    }
+}
